d03ff6246b4ecb2eb44bc6d8cb585711cca76ca8
mikebutts <mgbutts@gmail.com>
2015-09-01 17:28:26 -0400

    piece slides down, stops at bottom

--- tetris.js
+++ tetris.js
@@ -1,15 +1,18 @@
 controller = {
 
   init: function(){
+    //initialize the view
+    view.init();
     //testing
     model.init(); // creates 1 new piece
     view.drawPiece(model.currentPiece); //draws piece
-    setInterval(this.gameLoop, 1000);
+    setInterval(this.gameLoop, 10);
   },
 
   gameLoop: function(){
     view.clearBoard();        //clearBoard
     controller.movePiece();   //moves piece down
+    
     view.drawPiece(model.currentPiece); //
   },
 
@@ -28,6 +31,7 @@ model = {
 
   //Constructor
   SmallPiece: function(x,y){
+
       this.x = x;
       this.y = y;
       this.width = 40;
@@ -35,12 +39,22 @@ model = {
   },
 
   movePieceDown: function(){
-    model.currentPiece.y += 40;
+    if (model.checkCollision(model.currentPiece)){
+      model.currentPiece.y += 1;
+    }
   },
 
   dropPiece: function(){
     //drop piece all the way to bottom
     //check where lowest point before existing
+  },
+
+  checkCollision: function(piece){
+    if (piece.x < 0 || piece.x > view.canvas.width - piece.width || piece.y > view.canvas.height - piece.height - 1){
+        return false;
+    }else{
+      return true;
+    }
   },
 
   square: {
@@ -70,18 +84,20 @@ view = {
 
   clearBoard: function(){
     view.ctx.clearRect(0,0, view.canvas.width, view.canvas.height); //overwriting canvas
+
   },
 
   drawPiece: function(piece){
     // view.clearBoard();
     // view.ctx.clearRect(0,0, view.canvas.width, view.canvas.height);
-    view.ctx.rect(piece.x, piece.y, piece.width, piece.height);
-    view.ctx.stroke();
+    view.ctx.strokeRect(piece.x, piece.y, piece.width, piece.height);
+    // view.ctx.stroke();
+
   },
 
   changePieceColumn: function(event){
     if (event.which == 37 || event.which == 39){
-      model.piece.x += view.userMove[event.which] * 40;
+      model.currentPiece.x += view.userMove[event.which] * 40;
     }
 
     if (event.which == 40){
