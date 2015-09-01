c5af0240a35db69563027c0d6270ea749bf1fa71
mikebutts <mgbutts@gmail.com>
2015-09-01 14:13:39 -0400

    we have a small piece

--- tetris.js
+++ tetris.js
@@ -1,28 +1,51 @@
 controller = {
 
   init: function(){
-
+    var piece = new model.smallPiece(200,200)
+    view.drawPiece(piece);
   }
 
 };
 
 model = {
 
-  smallPiece: {
+  smallPiece: function(x,y){
+      this.x = x;
+      this.y = y;
+      this.width = 40;
+      this.height = 40;
+  },
 
+  square: {
+      // view.ctx.rect(0,20,40,40);
+      // view.ctx.stroke();
+      // view.ctx.rect(0,60,40,40);
+      // view.ctx.stroke();
+      // view.ctx.rect(40,20,40,40);
+      // view.ctx.stroke();
+      // view.ctx.rect(40,60,40,40);
+      // view.ctx.stroke();
   }
-
 };
 
 view = {
 
-  context: $("#board")[0].getContext("2d"),
+  ctx: $("#board")[0].getContext("2d"),
+
 
   //add listeners
   init: function(){
     $(document).keydown(function(event){
       view.changePieceColumn(event);
     });
+
+
+  },
+
+  drawPiece: function(piece){
+      view.ctx.rect(piece.x,piece.y,piece.width,piece.height);
+      view.ctx.stroke();
+
   },
 
   changePieceColumn: function(event){
